1aba4c22ac75b6d533bba2e5c8f3f2c002f37421
Lyon <lyon.b.lee@gmail.com>
2022-11-23 20:58:57 -0500

    Refactored and moved input codes into input.js from play.js

---
+++ input.js
@@ -0,0 +1,23 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+
+  const handleUserInput = function () {
+    // your code here
+
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+    
+  };
+
+  stdin.on("data", handleUserInput);
+
+  stdin.resume();
+  return stdin;
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,27 +1,7 @@
-const connect = require('./client.js');
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-
-  const handleUserInput = function () {
-    // your code here
-
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-    
-  };
-
-  stdin.on("data", handleUserInput);
-
-  stdin.resume();
-  return stdin;
-};
+const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
