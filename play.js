4ae9eb64f8a304eea74450df8dd0033dd2073af3
Lyon <lyon.b.lee@gmail.com>
2022-11-23 20:54:03 -0500

    added an ctrl+c input for termination

--- play.js
+++ play.js
@@ -1,4 +1,27 @@
 const connect = require('./client.js');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+
+  const handleUserInput = function () {
+    // your code here
+
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+    
+  };
+
+  stdin.on("data", handleUserInput);
+
+  stdin.resume();
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
