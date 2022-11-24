de2aa31c65ceed5d6ee1a06926d97ef60fac57c0
Lyon <lyon.b.lee@gmail.com>
2022-11-23 19:25:19 -0500

    played around with inputting up command to snake

--- client.js
+++ client.js
@@ -6,10 +6,15 @@ const connect = function () {
     port: '50541'// PORT number here,
   });
 
+  // const moveUp = () => {conn.write("Move: up")}; // callback function made for setTimeout & setInterval
+
   conn.on("connect", () => {
     // prints out when successfully connected
     console.log("Successfully connected to game server");
     conn.write("Name: BBB");
+    // conn.write("Move: up");      // Moves the snake up by one square
+    // setTimeout(moveUp, 1500);    // Moves the snake up by one, after 1.5 second
+    // setInterval(moveUp, 1500);   // Moves the snake up by one every 1.5 seconds
   })
 
   conn.on("data", () => {
